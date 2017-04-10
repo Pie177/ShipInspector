2a6f4adbac4e03bba815f8f1b21660562d81b3a8
svc-student <svc-student@student.directory.jccc>
2017-04-10 11:26:45 -0500

    Final Upload

--- Assets/Editor/ShipInspector.cs
+++ Assets/Editor/ShipInspector.cs
@@ -6,62 +6,81 @@ using UnityEditor;
 [CustomEditor(typeof(Ship))]
 public class ShipInspector : Editor
 {
-
+    int id = 0;
+    string[] selection = {"Ship", "Crew"};
     int pointPool = 80;
     Ship myTarget;
     public override void OnInspectorGUI()
     {
-        
 
-        EditorGUILayout.LabelField("Ship Stats", EditorStyles.boldLabel);
-        EditorGUILayout.BeginVertical("box");
-        EditorGUILayout.LabelField("Ship Stat Point Pool: " + pointPool.ToString());
-
-        EditorGUI.BeginChangeCheck();
-        myTarget.armor = EditorGUILayout.IntField("Armor", myTarget.armor);
-        if (EditorGUI.EndChangeCheck())
+        id = GUILayout.SelectionGrid(id, selection, 2);
+        if(id == 0)
         {
-            if (myTarget.armor < 10)
+            EditorGUILayout.LabelField("Ship Stats", EditorStyles.boldLabel);
+            EditorGUILayout.BeginVertical("box");
+            EditorGUILayout.LabelField("Ship Stat Point Pool: " + pointPool.ToString());
+
+            EditorGUI.BeginChangeCheck();
+            myTarget.armor = EditorGUILayout.IntField("Armor", myTarget.armor);
+            if (EditorGUI.EndChangeCheck())
             {
-                myTarget.armor = 10;
+                if (myTarget.armor < 10)
+                {
+                    myTarget.armor = 10;
+                }
+                if (myTarget.armor > 10 && (pointPool - myTarget.armor > -1))
+                {
+                    pointPool = 100 - myTarget.armor - myTarget.agility - myTarget.attack;
+                }
             }
-            if (myTarget.armor > 10 && (pointPool - myTarget.armor > -1))
+
+            EditorGUI.BeginChangeCheck();
+            myTarget.attack = EditorGUILayout.IntField("Attack", myTarget.attack);
+            if (EditorGUI.EndChangeCheck())
             {
-                pointPool = 100 - myTarget.armor - myTarget.agility - myTarget.attack;
+                if (myTarget.attack < 10)
+                {
+                    myTarget.attack = 10;
+                }
+                if (myTarget.attack > 10 && pointPool - myTarget.attack > -1)
+                {
+                    pointPool = 100 - myTarget.armor - myTarget.agility - myTarget.attack;
+                }
             }
+
+            EditorGUI.BeginChangeCheck();
+            myTarget.agility = EditorGUILayout.IntField("Agility", myTarget.agility);
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (myTarget.agility < 10)
+                {
+                    myTarget.agility = 10;
+                }
+                if (myTarget.agility > 10 && pointPool - myTarget.agility > -1)
+                {
+                    pointPool = 100 - myTarget.armor - myTarget.agility - myTarget.attack;
+                }
+            }
+            EditorGUILayout.Space();
+            myTarget.hitPoints = EditorGUILayout.IntField("Hit Points", myTarget.hitPoints);
+
+            serializedObject.Update();
+            SerializedProperty myGuns = serializedObject.FindProperty("shipGuns");
+            EditorGUILayout.PropertyField(myGuns, true);
+            serializedObject.ApplyModifiedProperties();
+            EditorGUILayout.EndVertical();
         }
 
-        EditorGUI.BeginChangeCheck();
-        myTarget.attack = EditorGUILayout.IntField("Attack", myTarget.attack);
-        if (EditorGUI.EndChangeCheck())
+        if(id == 1)
         {
-            if (myTarget.attack < 10)
-            {
-                myTarget.attack = 10;
-            }
-            if (myTarget.attack > 10 && pointPool - myTarget.attack > -1)
-            {
-                pointPool = 100 - myTarget.armor - myTarget.agility - myTarget.attack;
-            }
+            EditorGUILayout.LabelField("Crew", EditorStyles.boldLabel);
+            EditorGUILayout.BeginVertical("box");
+            serializedObject.Update();
+            SerializedProperty myCrew = serializedObject.FindProperty("crewMembers");
+            EditorGUILayout.PropertyField(myCrew, true);
+            serializedObject.ApplyModifiedProperties();
+            EditorGUILayout.EndVertical();
         }
-
-        EditorGUI.BeginChangeCheck();
-        myTarget.agility = EditorGUILayout.IntField("Agility", myTarget.agility);
-        if (EditorGUI.EndChangeCheck())
-        {
-            if (myTarget.agility < 10)
-            {
-                myTarget.agility = 10;
-            }
-            if (myTarget.agility > 10 && pointPool - myTarget.agility > -1)
-            {
-                pointPool = 100 - myTarget.armor - myTarget.agility - myTarget.attack;
-            }
-        }
-        EditorGUILayout.Space();
-        myTarget.hitPoints = EditorGUILayout.IntField("Hit Points", myTarget.hitPoints);
-        EditorGUILayout.EndVertical();
-
     }
 
     private void OnEnable()
